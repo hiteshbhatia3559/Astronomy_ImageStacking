4305489c254cee5d50b5f0feacdd5ca3c2b44cbe
irage <hiteshbhatia3559@gmail.com>
2019-04-05 17:05:22 +0530

    All done, pending README instructions and verbose output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import ImageChops
 import os
 from statistics import mean
 
@@ -20,10 +21,10 @@ def get_data(path_to_files):  # Get data from each file in dir
             mean_value = mean(maxs)
             # FWHM
             amp = np.max(main_data)
-            ypix, xpix = np.where(main_data==amp)
-            x_range = np.take(main_data,ypix[0],axis=0)
+            ypix, xpix = np.where(main_data == amp)
+            x_range = np.take(main_data, ypix[0], axis=0)
             y_range = np.take(main_data, xpix[0], axis=1)
-            half_max = amp/2
+            half_max = amp / 2
             d_x = x_range - half_max
             d_y = y_range - half_max
             indices_x = np.where(d_x > 0)[0]
@@ -32,17 +33,46 @@ def get_data(path_to_files):  # Get data from each file in dir
             # FMHM in x and y - simply counts how many pixels are within half the max in both directions
             width_x = len(indices_x)
             width_y = len(indices_y)
-            QF = mean_value/((width_x+width_y)/2)
-            dataset[file] = (file,QF)
+            QF = mean_value / ((width_x + width_y) / 2)
+            dataset[file] = (file, QF)
         except:
             print("Skipped " + file + " because it did not open!")
 
     return dataset
 
+if __name__ == '__main__':
 
-path_to_files = './Iota Cancri - Binary System-20190405T043457Z-001/Iota Cancri - Binary System/'
+    # EDIT PATH HERE
+    path_to_files = './Iota Cancri - Binary System-20190405T043457Z-001/Iota Cancri - Binary System/'
 
-dataset = get_data(path_to_files)
+    dataset = get_data(path_to_files)
 
-for item in list(dataset.values()):
-    print(item)
+    sorted_list = sorted(list(dataset.values()),key=lambda x: x[1])
+
+    files_to_merge = sorted_list[int(len(sorted_list)*(4/5)):]
+
+    filenames = []
+
+    for item in files_to_merge:
+        x,y = item
+        filenames.append(x)
+
+    image_concat = []
+    for image in filenames:
+        image_concat.append(fits.getdata(path_to_files+image))
+
+    final_image = np.zeros(shape=image_concat[0].shape)
+
+    for image in image_concat:
+        final_image+=image
+
+    # ONLY USABLE IN JUPYTER/PYCHARM
+    # SEE WHAT THE MERGED IMAGE LOOKS LIKE
+
+    # plt.imshow(final_image, cmap='gray', vmin=2.e3, vmax=3.e3)
+    # plt.colorbar()
+    # plt.show()
+    outfile = "stacked.fit"
+    hdu = fits.PrimaryHDU(final_image)
+    hdu.writeto(outfile,overwrite=True)
+    # Prints final file
