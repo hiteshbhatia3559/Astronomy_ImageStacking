a21d31543ffc4f0638bd36f3ab1d1b2e2f92042f
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-04-13 21:15:12 +0530

    Progress

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PIL import ImageChops
 import os
 from statistics import mean
 
+
 def get_data(path_to_files):  # Get data from each file in dir
     print("Getting data...\n")
     filenames = os.listdir(path_to_files)
@@ -42,12 +43,43 @@ def get_data(path_to_files):  # Get data from each file in dir
 
     return dataset
 
+
 def max_value(array):
     maximums = []
     for item in array:
-        if (max(item) < 20000) and (max(item) > 1000):
+        if (max(item) < 20000):
             maximums.append(max(item))
-    return max(maximums)
+    return maximums
+
+
+def find_best_fit(image_concat,max_values):
+    print("Finding best fit for alignment, this is a long process, please wait!\n")
+    final_image = np.zeros(shape=image_concat[0].shape)
+    x = list(range(-4, 4))
+    x.remove(0)
+    y = list(range(-4, 4))
+    y.remove(0)
+    list_of_finals = dict()
+    for a in x:
+        for b in y:
+            i = 0
+            for image in image_concat:
+                delta = max_values[0].index(max(max_values[0])) - max_values[i].index(max(max_values[i]))
+                # print(image)
+                if delta == 0:
+                    break
+                # print(delta)
+                new_image = np.roll(image, [int(delta / a), int(delta / b)], axis=(0, 1))
+                final_image += new_image
+                i += 1
+            list_of_finals[max(max_value(final_image))] = final_image
+
+    print(len(list(list_of_finals.keys())))
+    peak_image_brightness = max(list(list_of_finals.keys()))
+    # print(peak_image_brightness)
+    fitted_final_image = list_of_finals[peak_image_brightness]
+    return fitted_final_image
+
 
 if __name__ == '__main__':
 
@@ -65,23 +97,23 @@ if __name__ == '__main__':
     print("Top 20% images picked\nMerging now...\n")
 
     filenames = []
-
+    # Get all filenames in the folder
     for item in files_to_merge:
         x, y = item
         filenames.append(x)
 
-    print(filenames)
-
+    # Make a list of all 2d arrays that are images
     image_concat = []
+    max_values = []
     for image in filenames:
         image_data = fits.getdata(path_to_files + image)
-        cutout = Cutout2D(image_data,np.unravel_index(max_value(image_data),image_data.shape),size=100,copy=True).data
-        image_concat.append(cutout)
+        image_concat.append(image_data)
+        max_values.append(max_value(image_data))
+    print(len(max_values))
+    # Making a template array that is blank
 
-    final_image = np.zeros(shape=image_concat[0].shape)
+    final_image = find_best_fit(image_concat,max_values)
 
-    for image in image_concat:
-        final_image += image
     # ONLY USABLE IN JUPYTER/PYCHARM
     # SEE WHAT THE MERGED IMAGE LOOKS LIKE
 
@@ -89,9 +121,8 @@ if __name__ == '__main__':
     plt.colorbar()
     plt.show()
 
-    # outfile = "stacked.fit
+    # outfile = "stacked.fit"
     # hdu = fits.PrimaryHDU(final_image)
     # hdu.writeto(outfile, overwrite=True)
     # print("Please see {} for output".format(outfile))
     # # Prints final file
-
--- scratch.py
+++ scratch.py
@@ -1,36 +1,5 @@
-from astropy.io import fits
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import ImageChops
-import os
-from statistics import mean
-from astropy.nddata import Cutout2D
+x = list(range(-50,50))
 
-def max_value(array):
-    maximums = []
-    for item in array:
-        if (max(item) < 20000) and (max(item > 200)):
-            maximums.append(max(item))
-    return maximums
+x.remove(0)
 
-filenames = ['Autosave Image -0019.fit', 'Autosave Image -0051.fit', 'Autosave Image -0079.fit', 'Autosave Image -0062.fit', 'Autosave Image -0053.fit', 'Autosave Image -0030.fit', 'Autosave Image -0042.fit', 'Autosave Image -0069.fit', 'Autosave Image -0008.fit', 'Autosave Image -0050.fit', 'Autosave Image -0044.fit', 'Autosave Image -0004.fit', 'Autosave Image -0020.fit', 'Autosave Image -0002.fit', 'Autosave Image -0052.fit', 'Autosave Image -0040.fit', 'Autosave Image -0061.fit']
-path = 'Iota Cancri - Binary System-20190405T043457Z-001/Iota Cancri - Binary System/'
-
-ran = []
-
-for file in filenames[:4]:
-    data = fits.getdata(path+file)
-    ran.append(max_value(data))
-
-new = []
-
-for seq in ran:
-    delta = ran[0].index(max(ran[0])) - seq.index(max(seq))
-    new_seq = np.roll(seq,delta)
-    new.append(new_seq)
-
-for item in new:
-    plt.plot(item[80:190])
-
-plt.show()
-
+print(x)
