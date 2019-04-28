7cf85bae3f382fbf9dee96320dad6fed44958f66
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-04-29 00:32:47 +0530

    Done

--- psf.py
+++ psf.py
@@ -11,9 +11,15 @@ from find import find
 import matplotlib.pyplot as plt
 
 # load FITS image and specify PSF star coordinates
-data = fits.getdata('stacked.fit')
-i,j = np.unravel_index(data.argmax(), data.shape)
-image = Cutout2D(data, (j, i), size = 200).data
+data = fits.getdata('stacked1.fit')
+
+window = int(input("Size of object in pixel?\n"))
+
+
+# print(len(data))
+i, j = np.unravel_index(data.argmax(), data.shape)
+image = Cutout2D(data, (j, i), size=window*1.5).data
+# print(data.argmax())
 
 amp = np.max(image)
 ypix, xpix = np.where(image == amp)
@@ -26,31 +32,31 @@ indices_x = np.where(d_x > 0)[0]
 indices_y = np.where(d_y > 0)[0]
 
 width_x = len(indices_x)
-width_y= len(indices_y)
+width_y = len(indices_y)
 
-fwhm = (width_x+width_y)/2
+fwhm = (width_x + width_y) / 2
 
-rms = (np.std(image-image.mean(axis=0)))
+rms = (np.std(image - image.mean(axis=0)))
 
-x,y,flux,sharp,round = find(image,fwhm=fwhm,hmin=rms*3,verbose=False)
+x, y, flux, sharp, round = find(image, fwhm=fwhm, hmin=rms * 3, verbose=False)
 
-xpos,ypos = x,y
+xpos, ypos = x, y
 
 # run aper to get mags and sky values for specified coords
 mag, magerr, flux, fluxerr, sky, skyerr, badflag, outstr = \
     aper.aper(image, xpos, ypos, phpadu=1, apr=5, zeropoint=25,
-              skyrad=[40, 50], badpix=[0, 70000], exact=True,verbose=False)
+              skyrad=[40, 50], badpix=[0, 10000000000], exact=True, verbose=False)
 # use the stars at those coords to generate a PSF model
 gauss, psf, psfmag = \
     getpsf.getpsf(image, xpos, ypos,
                   mag, sky, 1, 1, np.arange(len(xpos)),
-                  fitrad=60, psfname='output_psf.fits',psfrad=70,verbose=False)
+                  fitrad=window/2, psfname='output_psf.fits', psfrad=int(window+20)/2, verbose=False)
 
-print("Amplitude of Gauss Function : "+str(gauss[0]))
-print("X-Axis Best Fit Offset : "+str(gauss[1]))
-print("Y-Axis Best Fit Offset : "+str(gauss[2]))
-print("Standard deviation of the gauss function on the X-axis : "+str(gauss[3]))
-print("Standard deviation of the gauss function on the Y-axis : "+str(gauss[4]))
+print("Amplitude of Gauss Function : " + str(gauss[0]))
+print("X-Axis Best Fit Offset : " + str(gauss[1]))
+print("Y-Axis Best Fit Offset : " + str(gauss[2]))
+print("Standard deviation of the gauss function on the X-axis : " + str(gauss[3]))
+print("Standard deviation of the gauss function on the Y-axis : " + str(gauss[4]))
 
 # plt.plot(gauss)
 # plt.show()
@@ -58,4 +64,3 @@ print("Standard deviation of the gauss function on the Y-axis : "+str(gauss[4]))
 # plt.imshow(data, cmap='gray', vmin=2.e3, vmax=3.e3)
 # plt.colorbar()
 # plt.show()
-
--- scratch.py
+++ scratch.py
@@ -1,8 +1,21 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
-data = fits.open('output_psf.fits')[0].data
+data = fits.open('stacked1.fit')[0].data
+data1 = fits.open('output_psf.fits')[0].data
 
-plt.imshow(data, cmap='gray', vmin=2.e3, vmax=3.e3)
+n1, n2 = 0,0
+total = 0
+for row in data1:
+    for item in row:
+        if item > 5000:
+            n1+=1
+        total+=1
+
+print(total/n1)
+
+
+
+plt.imshow(data, cmap='gray', vmin=1.e3, vmax=6.e3)
 plt.colorbar()
 plt.show()
